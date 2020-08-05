24a02cda6c621d08e8c8c63ac247429a5afa5859
st-ku <lecompt.by@gmail.com>
2020-08-05 03:42:36 +0300

    Added heroku deployment & ping task

--- src/main/java/ru/home/weathbot_telegramBot/WeathBotApplication.java
+++ src/main/java/ru/home/weathbot_telegramBot/WeathBotApplication.java
@@ -2,8 +2,11 @@ package ru.home.weathbot_telegramBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class WeathBotApplication {
 
     public static void main(String[] args) {
