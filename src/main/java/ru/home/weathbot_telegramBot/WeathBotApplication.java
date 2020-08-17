045d1ec9c3a75b2a582e8eb05ab708501e3ae5b5
st-ku <lecompt.by@gmail.com>
2020-08-17 17:59:12 +0300

    pingTasker deleted

--- src/main/java/ru/home/weathbot_telegramBot/WeathBotApplication.java
+++ src/main/java/ru/home/weathbot_telegramBot/WeathBotApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-
 public class WeathBotApplication {
 
     public static void main(String[] args) {
