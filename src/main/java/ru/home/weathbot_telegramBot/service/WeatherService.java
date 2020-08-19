8d277c746afa285935897bd7acff71a2677001bb
st-ku <lecompt.by@gmail.com>
2020-08-19 04:55:25 +0300

    emoji and wind display

--- src/main/java/ru/home/weathbot_telegramBot/service/WeatherService.java
+++ src/main/java/ru/home/weathbot_telegramBot/service/WeatherService.java
@@ -1,15 +1,35 @@
 package ru.home.weathbot_telegramBot.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
 import ru.home.weathbot_telegramBot.model.weatherModel.FullWeather;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
+@Slf4j
 public class WeatherService {
     private ReplyMessagesService messagesService;
+  //  @Value("classpath:resources/icons.json")
+  //  Resource json;
 
     public WeatherService(ReplyMessagesService messagesService) {
         this.messagesService = messagesService;
@@ -33,9 +53,14 @@ public class WeatherService {
        }
        return true;
     }
-
+    @SneakyThrows
     public String formatWeather(FullWeather fullWeather) {
-        String result = String.format(messagesService.getReplyText("reply.weatherPattern1"),fullWeather.getName(),fullWeather.getMain().getTemp(),fullWeather.getMain().getFeels_like(), Arrays.stream(fullWeather.getWeather()).iterator().next().getDescription());
-        return result ;
+        return String.format(messagesService.getReplyText("reply.weatherPattern1"),fullWeather.getName(),fullWeather.getMain().getTemp(),fullWeather.getMain().getFeels_like(),createEmojiByCode(Arrays.stream(fullWeather.getWeather()).iterator().next().getIcon()), Arrays.stream(fullWeather.getWeather()).iterator().next().getDescription(), fullWeather.getWind().getSpeed());
+    }
+    @SneakyThrows
+    private String createEmojiByCode(String code)  {
+        String json= StreamUtils.copyToString( new ClassPathResource("icons.json").getInputStream(), Charset.defaultCharset()  );
+        Map<String, String> map = new Gson().fromJson(json, Map.class);
+        return map.get(code);
     }
 }
